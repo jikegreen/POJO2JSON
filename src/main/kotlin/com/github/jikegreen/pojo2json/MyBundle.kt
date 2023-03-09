ac4541657a81b9da2accb482caf269b1a3c39493
GitHub Action <action@github.com>
2023-03-09 10:13:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/jikegreen/pojo2json/MyBundle.kt
+++ src/main/kotlin/com/github/jikegreen/pojo2json/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jikegreen.pojo2json
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jikegreen/pojo2json/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jikegreen/pojo2json/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jikegreen.pojo2json.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jikegreen.pojo2json.MyBundle
 
 class MyApplicationService {
 
